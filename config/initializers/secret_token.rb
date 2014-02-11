5e78690379aca60981ece48317cef9db96385009
pecomeri <takashina.k520@gmail.com>
2014-02-12 00:07:39 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeatherMailR::Application.config.secret_key_base = 'ca2e023763ef5bf6c787df98ad71ec4e3647398cbe8a08ab7a7fd8da888993f48d7236a851915a1256c3e79cd7f45e24ac28c602f1b74f442bc501b2c84be934'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeatherMailR::Application.config.session_store :cookie_store, key: '_weatherMailR_session'
