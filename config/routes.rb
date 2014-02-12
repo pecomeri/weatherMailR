f7b8e24d38dad174aa01a84c936df85a757264b0
pecomeri <takashina.k520@gmail.com>
2014-02-12 19:10:35 +0900

    Make user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 WeatherMailR::Application.routes.draw do
   resources :users
+  root 'static_pages#home'
+  match '/signup', to: 'users#new', via:'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
