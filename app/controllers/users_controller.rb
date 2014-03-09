0bee20f4d18ecbd476ee6443ab8e50b9ee45db46
pecomeri <takashina.k520@gmail.com>
2014-03-09 18:02:46 +0900

    make microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   # GET /users/new
@@ -65,14 +66,6 @@ class UsersController < ApplicationController
     end
 
     # Before actions
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        flash[:danger] = "Please sign in."
-        redirect_to signin_url
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
